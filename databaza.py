1d87a47fc2999d90addff6c9c1e80dc3ffdea346
zuzana-gaz <nzexp12@gmail.com>
2026-09-25 19:53:55 +0200

    precvicovanie select prikazu

--- databaza.py
+++ databaza.py
@@ -2,19 +2,9 @@ import sqlite3
 
 connection = sqlite3.connect("mesto_zilina.db")
 cursor = connection.cursor()
-cursor.execute(
-    """
-    INSERT INTO zmluvy (kod, cena)
-    VALUES ('Z01', 12000.0);
-"""    
-)
-cursor.execute(
-    """
-    INSERT INTO zmluvy (kod, cena)
-    VALUES ('Z02', 15000.0);
-"""
-)
 
-connection.commit()
+cursor.execute("SELECT kod FROM zmluvy")
+print(cursor.fetchone())
+print(cursor.fetchall())
+
 connection.close()
-
